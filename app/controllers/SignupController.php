3dd7c8a196a4e66bf257550b4bc40db317ac1900
Bullet <volodya.grab@yandex.ru>
2016-12-20 16:27:39 +0500

    Initial commit

---
+++ app/controllers/SignupController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: grabvn
+ * Date: 20.12.2016
+ * Time: 11:54
+ */
+
+class SignupController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
