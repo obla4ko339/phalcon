b4e2c79d1da11bad89d9db57d3d9b534258d920c
Bullet <volodya.grab@yandex.ru>
2016-12-27 18:02:03 +0500

    27122016

---
+++ app/controllers/AdressController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: grabvn
+ * Date: 26.12.2016
+ * Time: 16:44
+ */
+class AdressController extends \Phalcon\Mvc\Controller
+{
+    public function indexAction(){
+
+    }
+
+    public function adressAction(){
+        echo "H";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,19 @@
 <?php
+use Phalcon\Mvc\Router;
+
 try {
+
+    $router = new Router();
+
+    $router->add(
+        "/phalcon/adress",
+        [
+            "controller"=>"signup",
+            "action"=>"register",
+        ]
+    );
+
+
 
     //Register an autoloader
     $loader = new \Phalcon\Loader();
@@ -10,6 +24,14 @@ try {
 
     //Create a DI
     $di = new Phalcon\DI\FactoryDefault();
+
+    $di->set(
+        "IndexController",
+        function(){
+            $component = new Phalcon\Mvc\User\Component();
+            return $component;
+        }
+    );
 
     $di->set('db', function(){
        return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
@@ -26,6 +48,10 @@ try {
         $view->setViewsDir('../app/views/');
         return $view;
     });
+
+
+
+
 
     //Handle the request
     $application = new \Phalcon\Mvc\Application($di);
